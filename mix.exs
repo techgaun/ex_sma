e56ba8cdd9bce5ff140313b6f87e05aa85d4eec7
techgaun <coolsamar207@gmail.com>
2016-07-17 00:18:19 -0500

    mix updates

--- mix.exs
+++ mix.exs
@@ -5,8 +5,11 @@ defmodule Sma.Mixfile do
     [app: :sma,
      version: "0.1.0",
      elixir: "~> 1.2",
+     source_url: "https://github.com/techgaun/ex_sma",
+     description: "Simple moving average calculator in elixir",
+     package: package,
      test_coverage: [tool: ExCoveralls],
-    preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -29,6 +32,18 @@ defmodule Sma.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:excoveralls, "~> 0.5", only: :test}]
+    [{:earmark, "~> 0.1", only: :dev},
+    {:ex_doc, "~> 0.11", only: :dev},
+    {:excoveralls, "~> 0.5", only: :test}]
+  end
+
+  defp package do
+    [
+      maintainers: [
+        "Samar Acharya"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/techgaun/ex_sma"}
+    ]
   end
 end
